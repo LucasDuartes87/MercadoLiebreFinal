a7e48f36fb6a5e09a080766e741bb60947dbfa96
LucasDuartes <ldd87@outlook.com>
2022-04-21 17:46:14 -0300

    subiendo el repo

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require ('express');
+const app = express ();
+const path = require('path');
+
+/*app.listen(3000,() =>
+console.log('servidor corriendo')
+);*/
+const publicPath = 'public';
+app.use( express.static(publicPath));
+
+// Ruta raiz
+
+app.get('/', (req, res)=> {
+    res.sendFile(path.join(__dirname , '/views/home.html'));
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+app.post('/', (req, res)=> {
+    res.sendFile(path.join(__dirname , '/views/home.html'));
+});
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log('Servidor funcionando puerto: ' + process.env.PORT);
+});
